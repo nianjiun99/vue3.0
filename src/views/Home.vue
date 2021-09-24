4116d109575bd8d33efccfe28a78789307832edf
weilc <2262015678@qq.com>
2021-09-24 14:55:26 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="add()" :class="showEdit.editvalue ? 'redBg' : 'redColor'" >
+      点我点我{{ aaa.title }}
+    </button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { reactive, watch } from "vue";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+  props: {
+    title: Number,
+  },
+  setup(props, context) {
+    console.log(props);
+    let showEdit = reactive({editvalue:false,isis:false});
+    const aaa = reactive(props);
+    // add(()=>{
+    //   console.log(context,"abc")
+    // })
+    function add() {
+      context.emit("showdown");
+      
+      // watch(aaa.title,(newValue,oldValue)=>{
+      //   console.log(newValue,oldValue)
+      // })
+    }
+    watch(
+      () => aaa.title,
+      (newValue, oldValue) => {
+        if (newValue > 110) {
+          showEdit.editvalue = true;
+          showEdit.isis = true
+        }
+        console.log(newValue, oldValue, showEdit, "sjow");
+      }
+    );
+    return {
+      aaa,
+      add,
+      showEdit,
+    };
+  },
+};
 </script>
+<style scoped>
+.redBg {
+  background: red;
+}
+</style>
---
+++ src/views/List.vue
@@ -0,0 +1,65 @@
+<template>
+  <home :title="title" @showdown="showdown"></home>
+  <!-- <ul>
+    <li v-for="(item, index) in defaultData.tableData" :key="index">
+      {{ item.name }}
+    </li>
+  </ul> -->
+  <el-table :data="defaultData.tableData" border style="width: 100%">
+    <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+    <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+    <el-table-column prop="address" label="地址"> </el-table-column>
+  </el-table>
+</template>
+
+<script>
+import { reactive, onMounted, ref,  } from "vue";
+import home from "./Home.vue";
+export default {
+  components: {
+    home,
+  },
+  setup() {
+    //reactive 定义对象    ref 定义常量
+
+    const title = ref(100);
+    const defaultData = reactive({
+      tableData: [
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+      ],
+    });
+    function showdown() {
+      const objValue = {
+        date: title.value,
+        name: `校长${title.value}`,
+        address: `上海市普陀区金沙江路 ${title.value} 弄`,
+      };
+      title.value++;
+      defaultData.tableData.push(objValue);
+    }
+    onMounted(() => {
+      defaultData.tableData[0].name = "张三";
+      console.log(defaultData.tableData, "75456");
+    });
+
+    // watch(title, (newValue, oldValue) => {
+    //   console.log(newValue, oldValue, "76565465");
+    // });
+    return { defaultData, showdown, title };
+  },
+};
+</script>
